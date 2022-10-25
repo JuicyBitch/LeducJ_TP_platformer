e2b3cf753cabfbda8ba727829bf7fb6fdd08b553
JuicyBitch <joe.leduc.1@umontreal.ca>
2022-10-25 17:07:48 -0400

    First Commit

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth = 3;
+    public int currentHealth;
+
+    public float invincibilityTimeAfterHit = 3f;
+    public float invincibilityFlashDelay = 0.2f;
+
+    public bool isInvincible = false;
+
+    public SpriteRenderer graphics;
+
+    public HealthBar healthBar;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            TakeDamage(1);
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        if (!isInvincible)
+        {
+            currentHealth -= damage;
+            healthBar.SetHealth(currentHealth);
+            isInvincible = true;
+            StartCoroutine(InvincibilityFlash());
+            StartCoroutine(HandleInvincibilityDelay());
+        }
+        
+    }
+
+    public IEnumerator InvincibilityFlash()
+    {
+        while (isInvincible)
+        {
+            graphics.color = new Color(1f, 1f, 1f, 0f);
+            yield return new WaitForSeconds(invincibilityFlashDelay);
+            graphics.color = new Color(1f, 1f, 1f, 1f);
+            yield return new WaitForSeconds(invincibilityFlashDelay);
+        }
+    }
+
+    public IEnumerator HandleInvincibilityDelay()
+    {
+        yield return new WaitForSeconds(invincibilityTimeAfterHit);
+        isInvincible = false;
+    }
+}
---
+++ Assets/Scripts/WeakPoint.cs
@@ -0,0 +1,15 @@
+
+using UnityEngine;
+
+public class WeakPoint : MonoBehaviour
+{
+    public GameObject objectToDestroy;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Destroy(objectToDestroy);
+        }
+    }
+}
